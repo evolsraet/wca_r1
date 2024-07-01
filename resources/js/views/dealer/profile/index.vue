dd043fd51d976d20bc874e8c9beba6a5e39fcaa6
jiyoon612 <wldbsvk612@gmail.com>
2024-07-01 11:06:12 +0900

    test

--- resources/js/views/dealer/profile/index.vue
+++ resources/js/views/dealer/profile/index.vue
@@ -84,7 +84,7 @@
                 </div>
                 <span class="tc-light-gray">딜러가 낙찰받은 매물들이에요.</span>
                 <div v-if="filteredViewBids.length == 0">
-                    <p class="tc-red bold-18-font my-2">낙찰 받은 매물이 없습니다.</p>
+                    <p class="tc-red bold-18-font my-2">낙찰 받은 매물이 없습니다</p>
                 </div>
                 <div v-if="filteredViewBids.length > 0">
                     <div class="complete-car mt-3"  v-for="bid in filteredViewBids" :key="bid.id">
