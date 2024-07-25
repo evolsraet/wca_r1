7727647a0abccd7a4e3cd5c7cfee2fb74840ef61
evolsraet <ks1995@gmail.com>
2024-07-25 14:25:14 +0900

    fix : 유저 권한변경 업데이트 에러

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -133,6 +133,9 @@ class UserService
 
             $item = $item->findOrFail($id);
 
+            // print_r($item->dealer()->exists() ? 'true' : 'false');
+            // die();
+
             $this->modifyAuth($item);
 
             $data = $request->input('user');
@@ -146,16 +149,24 @@ class UserService
             ])->validate();
 
             $data = $this->beforeData($data);
-            // print_r($data);
-            // die();
             $data['dealer'] = $this->checkJson($request->input('dealer'));
 
+            // 역할 지정
+            // 기본 패키지는 복수지만, 현 서비스에서는 하나만 지정한다
+            if (auth()->user()->hasPermissionTo('act.admin') && $data) {
+                if (isset($data['role'])) {
+                    // 이름으로 역할 찾기
+                    $role = Role::where('name', $data['role'])->first();
+                    // error - findByName($name, $guard = 디폴트 auth.defaults.guard) 인데, 어느순간 sanctum 으로 변경되어 사용이 불가하다.
+                    // $role = Role::findByName($data['role']);
+
+                    $item->syncRoles($role);
+                }
+            }
 
 
             // print_r('업데이트 메소드');
             // print_r($request->all());
-            // print_r($data);
-            // die();
 
             // $dealerData = null;
             // $dealerData = $this->checkJson($request->input('dealer'));
@@ -168,32 +179,44 @@ class UserService
             $model = new User;
 
             if ($item->hasRole('dealer') && $data['dealer']) {
+
+                // $validator = Validator::make($data['dealer'], [
+                //     'name' => 'required',
+                //     'phone' => 'required',
+                //     'birthday' => 'required',
+                //     'company' => 'required',
+                //     'company_duty' => 'required',
+                //     'company_post' => 'required',
+                //     'company_addr1' => 'required',
+                //     'company_addr2' => 'required',
+                //     'introduce' => 'required',
+                // ]);
+                // // 유효성 검사 실패 시
+                // if ($validator->fails()) {
+                //     return response()->api(null, '필수값이 누락되었습니다.', 'fail', 422, ['errors' => $validator->errors()]);
+                // }
+
                 // $item->dealer()->updateOrCreate 는 제대로 기능안함. create 만 하려고함
-                if ($item->dealer()) {
+
+                if ($item->dealer()->exists()) {
+                    print_r(
+                        [
+                            '업데이트',
+                            '데이터' => $data['dealer'],
+                        ]
+                    );
+                    die();
                     // 기존 Dealer 업데이트
                     $item->dealer()->update($data['dealer']);
                     // TODO: 변경불가 사항 업데이트 시 상태변경 (누구에게 알림?)
                 } else {
-                    $validator = Validator::make($data['dealer'], [
-                        'name' => 'required',
-                        'phone' => 'required',
-                        'birthday' => 'required',
-                        'company' => 'required',
-                        'company_duty' => 'required',
-                        'company_post' => 'required',
-                        'company_addr1' => 'required',
-                        'company_addr2' => 'required',
-                        'receive_post' => 'required',
-                        'receive_addr1' => 'required',
-                        'receive_addr2' => 'required',
-                        'introduce' => 'required',
-                    ]);
-
-                    // 유효성 검사 실패 시
-                    if ($validator->fails()) {
-                        return response()->api(null, null, 'fail', 422, ['errors' => $validator->errors()]);
-                    }
-
+                    // print_r(
+                    //     [
+                    //         '생성',
+                    //         '데이터' => $data['dealer'],
+                    //     ]
+                    // );
+                    // die();
                     // 새 Dealer 생성
                     $item->dealer()->create($data['dealer']);
                     // TODO: 알림 : 누구에게?
@@ -222,19 +245,6 @@ class UserService
                         // $files_one 배열에 있는 파일들은 자동으로 기존 파일을 삭제하고 새 파일로 대체됨
                         $file_result[] = $item->addMedia($file)->preservingOriginal()->toMediaCollection($key);
                     }
-                }
-            }
-
-            // 역할 지정
-            // 기본 패키지는 복수지만, 현 서비스에서는 하나만 지정한다
-            if (auth()->user()->hasPermissionTo('act.admin') && $data) {
-                if (isset($data['role'])) {
-                    // 이름으로 역할 찾기
-                    $role = Role::where('name', $data['role'])->first();
-                    // error - findByName($name, $guard = 디폴트 auth.defaults.guard) 인데, 어느순간 sanctum 으로 변경되어 사용이 불가하다.
-                    // $role = Role::findByName($data['role']);
-
-                    $item->syncRoles($role);
                 }
             }
 
