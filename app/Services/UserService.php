6028cb056b4db0bdc68c6b4772f6d1e5188cfefe
evolsraet <ks1995@gmail.com>
2024-07-29 17:51:42 +0900

    user read auth

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -2,9 +2,11 @@
 
 namespace App\Services;
 
-use App\Models\Role;
+use App\Models\Bid;
 // use Spatie\Permission\Models\Role;
+use App\Models\Role;
 use App\Models\User;
+use App\Models\Auction;
 use App\Traits\CrudTrait;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -314,20 +316,37 @@ class UserService
     protected function middleProcess($method, $request, $item, $id = null)
     {
         if ($method === 'index') {
-            // $item->with('role');
-            // print_r(auth()->user()->hasPermissionTo);
-            // die();
-            if (auth()->check() && !auth()->user()->hasPermissionTo('act.admin')) {
-                $item->where('id', auth()->user()->id);
-                $item->role(['user', 'dealer']);
-            }
+            $this->readAuth($item);
         } elseif ($method === 'show') {
-            if (auth()->check() && !auth()->user()->hasPermissionTo('act.admin')) {
-                $item->where('id', auth()->user()->id);
-                $item->role(['user', 'dealer']);
-            }
+            $this->readAuth($item);
         } elseif ($method === 'destroy') {
             $this->modifyAuth($item);
+        }
+    }
+
+    protected function readAuth($item)
+    {
+        $item->with('roles');
+
+        if (auth()->check() && !auth()->user()->hasPermissionTo('act.admin')) {
+            $item->role(['user', 'dealer']);
+
+            // 딜러는 본인만, 유저는 본인과 연관된 딜러만
+            if (auth()->user()->can('act.dealer')) {
+                $item->where('id', auth()->user()->id);
+            } elseif (auth()->user()->can('act.user')) {
+                // 본인 이거나 내 매물과 관련된 딜러들만
+                $auction_ids = Auction::where('user_id', auth()->user()->id)->pluck('id');
+                $bid_user_ids = Bid::whereIn('auction_id', $auction_ids)->pluck('user_id');
+
+                // Log::info($auction_ids);
+                // Log::info($bid_user_ids);
+
+                $item->where(function ($query) use ($bid_user_ids) {
+                    $query->where('id', auth()->user()->id)
+                        ->orWhereIn('id', $bid_user_ids);
+                });
+            }
         }
     }
 }
