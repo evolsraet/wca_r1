33c2e996be7cf427b82b2119f86d08f3b2ebda26
jiyoon612 <wldbsvk612@gmail.com>
2024-08-08 13:11:29 +0900

    탁송일 입력 딜러 유저 구분

--- resources/js/views/auction/Details/details.vue
+++ resources/js/views/auction/Details/details.vue
@@ -510,9 +510,11 @@
               <p class="text-secondary opacity-50">낙&nbsp;&nbsp;  찰&nbsp;&nbsp;  액 : <span class="tc-red ms-1">{{auctionDetail.data.final_price}}</span></p>
               <p class="text-secondary opacity-50">입금&nbsp;&nbsp;은행 :<span class="tc-red ms-1">({{auctionDetail.data.bank}}은행) {{auctionDetail.data.account}}</span></p>
               <p class="text-secondary opacity-50">탁&nbsp;&nbsp; 송&nbsp;&nbsp; 일 : <span class="tc-red ms-1">2024년 6월 26일 오후 6:12</span></p>
-              <button class="btn primary-btn w-100 my-2" @click="showModal2">
-                탁송일 입력하기
-              </button>
+              <div v-if="isUser && auctionDetail.data.status == 'chosen'">
+                <button class="btn primary-btn w-100 my-2" @click="showModal2">
+                  탁송일 입력하기
+                </button>
+              </div>
             </div>
             <div>
               <button class="border-6 btn-fileupload my-4 shadow02 text-secondary opacity-50" @click="AttachedInform">딜러 첨부파일</button>
