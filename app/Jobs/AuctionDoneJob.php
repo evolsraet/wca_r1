1810daab716ca6952e28fb8eaca9206bfb62ea44
waanee <sw_kim87@naver.com>
2025-01-26 15:48:23 +0900

    수수료 알림부분 수정작업

--- app/Jobs/AuctionDoneJob.php
+++ app/Jobs/AuctionDoneJob.php
@@ -12,6 +12,9 @@ use App\Notifications\AuctionDoneNotification;
 use App\Notifications\AligoNotification;
 use App\Models\Auction;
 use App\Notifications\Templates\NotificationTemplate;
+use App\Http\Controllers\Api\PaymentController;
+use Illuminate\Support\Facades\Log;
+
 class AuctionDoneJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
@@ -69,8 +72,25 @@ class AuctionDoneJob implements ShouldQueue
 
             case 'dealer':
 
+                $dealer = User::find($this->user);
+                $auction_id = $this->auction;
+                $randomPrefix = substr(str_shuffle("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 8);
+                $randomSuffix = substr(str_shuffle("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 3);
+                $moid = $randomPrefix . $auction_id . $randomSuffix; // 앞에8자리 랜덤 + bid_id + 뒤에 랜덤 3자리 조합 
+                $VbankExpDate = date('Ymd', strtotime('+1 day'));
+                $VbankExpTime = date('His');
                 // 나이스페이먼츠 API 가상계좌 번호 발급 
+                $data = array(
+                    'VbankAccountName'=>$dealer->name, 
+                    'VbankExpDate'=>$VbankExpDate, 
+                    'VbankExpTime'=>$VbankExpTime, 
+                    'Amt'=>$auction->final_price, // $auction->final_price
+                    'Moid'=>$moid);
 
+                $account = (new PaymentController())->checkOverPayment($data);
+
+                $VbankExpDateTrans = date('Y-m-d', strtotime($account['data']['VbankExpDate']));
+                $VbankExpTimeTrans = date('H:i:s', strtotime($account['data']['VbankExpTime']));
                 // 알림 전송 데이터
                 $data = [
                     'title' => '경매가 완료되었습니다.',
@@ -80,9 +100,9 @@ class AuctionDoneJob implements ShouldQueue
                     'status11' => $auction->total_fee ? $auction->total_fee : '20000', // 총 입금액 (수수료. 진단비를 자동으로 계산 하는 부분 만들어서 연동 )
                     'status12' => $auction->success_fee ? $auction->success_fee : '20000', // 수수료 
                     'status13' => $auction->diag_fee ? $auction->diag_fee : '20000', // 진단비
-                    'status14' => '123-1234-1234', // 계좌번호 (가상계좌번호 발급)
-                    'status15' => '예금주', // 예금주
-                    'status16' => '2025-01-16(수) 00시', // 입금기한
+                    'status14' => $account['data']['VbankNum'], // 계좌번호 (가상계좌번호 발급)
+                    'status15' => $account['data']['VbankAccountName'], // 예금주
+                    'status16' => $VbankExpDateTrans.'('.$VbankExpTimeTrans.')', // 입금기한
                     'footerMsg' => '* 수수료 미입금시 클레임이 불가합니다.',
                     //'link' => $baseUrl, // 결제 페이지 링크를 전송? 
                     //'linkTitle' => '수수료결제'
