785adbee540f21c13bc755856eadb03faf13293a
waanee <sw_kim87@naver.com>
2025-05-20 12:55:26 +0900

    up

--- app/Services/ApiRequestService.php
+++ app/Services/ApiRequestService.php
@@ -39,12 +39,8 @@ class ApiRequestService
                     'body'   => $response->body(),
                 ];
 
-                $this->logErrorToDb($method, $url, $params, $response->body(), $logContext);
-
-                Log::warning("[$logContext] {$method} 실패", $errorData);
-
                 if ($throwOnError) {
-                    throw new Exception("API 요청 실패 했습니다.");
+                    throw new Exception("API 요청 실패 했습니다.", 500);
                 }
 
                 return null;
@@ -53,7 +49,7 @@ class ApiRequestService
             // 성공 로그
             $result = $response->json();
 
-            Log::info("[$logContext] {$method} 성공", [
+            Log::info("{$method} 성공 : [$logContext] ", [
                 'url'    => $url,
                 'params' => $params,
                 'result' => $result,
@@ -62,12 +58,20 @@ class ApiRequestService
             return $result;
 
         } catch (Exception $e) {
-            Log::error("[$logContext] 예외 발생", [
+            $msg = "{$method} 실패 : [$logContext]";
+            $context = [
                 'message'    => $e->getMessage(),
                 'error_data' => $errorData,
                 'trace'      => $e->getTraceAsString(),
-            ]);
+            ];
 
+            $errorLevel = 'warning';
+            if( $e->getCode() == 500 ) {
+                Log::error($msg, $context);
+            } else {
+                Log::warning($msg, $context);
+            }
+            
             $this->logErrorToDb($method, $url, $params, $errorData['body'] ?? null, $logContext, $e);
 
             if ($throwOnError) {
