26c4ead1a5c729764778c4001c1639f4839ae235
waanee <sw_kim87@naver.com>
2025-05-27 23:21:03 +0900

    npm 취약점 해결1

--- resources/v1/js/store/index.js
+++ resources/v1/js/store/index.js
@@ -1,27 +1,29 @@
 import { createStore } from 'vuex'
-import createPersistedState from 'vuex-persistedstate'
 import auth from './auth'
 import lang from './lang'
-import cancelAttempted from './cancelAttempted';
-//import lib_storage from '../store/lib_storage' 
+import cancelAttempted from './cancelAttempted'
+// import lib_storage from '../store/lib_storage'
 import enums from './enums'
 import fields from './fields'
 
 const store = createStore({
-    state: {
-        commercialName: '위카' // 초기 상업명 설정
-      },
-    plugins:[
-        createPersistedState()
-    ],
-    modules:{
-        auth,
-        lang,
-        cancelAttempted,
-        //lib_storage, //저장소 라이브러리 명
-        enums,
-        fields,
+  state: {
+    commercialName: localStorage.getItem('commercialName') || '위카'
+  },
+  mutations: {
+    setCommercialName(state, name) {
+      state.commercialName = name
+      localStorage.setItem('commercialName', name)
     }
+  },
+  modules: {
+    auth,
+    lang,
+    cancelAttempted,
+    // lib_storage,
+    enums,
+    fields
+  }
 })
 
 export default store
