2e7537eb28119e7e6493cf1043cefabe64c14bb1
waanee <sw_kim87@naver.com>
2025-06-02 22:01:59 +0900

    v2 라우트 별도 관리

---
+++ routes/v2.php
@@ -0,0 +1,131 @@
+<?php
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\ResetPasswordController;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\BoardController;
+
+
+// v2 Routes
+Route::get('/', function () {
+    // return view('v2.pages.home');
+    $user = Auth::user();
+    if ($user) {
+        return view('v2.pages.userMain');
+    } else {
+        return view('v2.pages.home');
+    }
+})->name('home');
+
+Route::get('/test', function () {
+    return view('v2.pages.test');
+});
+
+Route::get('/introduce', function () {
+    return view('v2.pages.introduce');
+});
+
+Route::get('/test/upload', function () {
+    return view('v2.test.upload');
+});
+
+Route::get('/user-main', function () {
+    return view('v2.pages.userMain');
+});
+
+// docs 라우트 / docs 폴더에 html 파일을 찾아서 보여줌
+Route::get('/docs/{doc}', function ($doc) {
+    $filePath = resource_path("v2/docs/{$doc}.html");
+
+    if (!file_exists($filePath)) {
+        abort(404, '문서를 찾을 수 없습니다.');
+    }
+
+    $html = file_get_contents($filePath);
+
+    // 쿼리 파라미터가 ?raw=1 인 경우 원본 HTML 출력
+    if (request()->query('raw') === '1') {
+        $html = preg_replace('/<h1[^>]*>.*?<\/h1>/s', '', $html);
+        return response($html)->header('Content-Type', 'text/html');
+    }
+
+    return view('v2.pages.docs', [
+        'html' => $html,
+    ]);
+})->where('doc', '[A-Za-z0-9\-_]+')->name('docs.show');
+
+Route::prefix('auction')->group(function () {
+    Route::get('/', function () {
+        return view('v2.pages.auction.auctionList');
+    })->name('auction.list');
+
+    Route::get('/{id}', function ($id) {
+        return view('v2.pages.auction.auctionDetail', ['id' => $id]);
+    })->name('auction.detail');
+});
+
+Route::post('login', [AuthenticatedSessionController::class, 'login']);
+Route::post('logout', [AuthenticatedSessionController::class, 'logout']);
+
+Route::get('/sell', function () {
+    return view('v2.pages.sell');
+})->name('sell');
+
+Route::get('/style-guide', function () {
+    return view('v2.pages.styleGuide');
+})->name('style-guide');
+
+// 인증 관련 라우트
+Route::middleware('guest')->group(function () {
+    Route::get('/login', function () {
+        return view('v2.auth.login');
+    })->name('login');
+
+    Route::get('/register', function () {
+        return view('v2.auth.register');
+    })->name('register');
+
+    // 비밀번호 재설정 라우트
+    Route::get('/forgot-password', function () {
+        return view('v2.auth.forgot-password');
+    })->name('password.request');
+
+    Route::post('/forgot-password', [ForgotPasswordController::class, 'sendResetLinkEmail'])
+        ->name('password.email');
+
+    Route::get('/reset-password/{token}', function ($token) {
+        return view('v2.auth.reset-password', ['token' => $token]);
+    })->name('password.reset');
+
+    Route::post('/reset-password', [ResetPasswordController::class, 'reset'])
+        ->name('password.update');
+});
+Route::post('logout', [AuthenticatedSessionController::class, 'logout']);
+Route::get('modify', function () {
+    return view('v2.auth.register');
+})->name('modify');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+        ->name('logout');
+});
+
+// 게시판 라우트 그룹
+Route::prefix('board/{boardId}')->name('board.')->group(function () {
+    // 목록 페이지
+    Route::get('/', [BoardController::class, 'list'])->name('list');
+
+    // 상세 페이지
+    Route::get('/view/{articleId}', [BoardController::class, 'view'])->name('view');
+
+    // 글쓰기/수정 페이지
+    Route::get('/form/{articleId?}', [BoardController::class, 'form'])->name('form');
+});
+
