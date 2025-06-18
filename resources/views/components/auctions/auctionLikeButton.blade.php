34426ce93cf05adf33a3f3e53c6b1e2f9d13162d
waanee <sw_kim87@naver.com>
2025-06-18 22:07:50 +0900

    좋아요 기능 오류 수정

--- resources/views/components/auctions/auctionLikeButton.blade.php
+++ resources/views/components/auctions/auctionLikeButton.blade.php
@@ -1,6 +1,7 @@
 {{-- 좋아요 기능 --}}
 <div class="position-absolute top-0 end-0 z-2" x-show="window.userRole === 'dealer'">
     {{-- 이 위치에 좋아요 기능 컴포넌트 추가 --}}
+
     <div>
         <button
             class="btn fs-2"
@@ -8,16 +9,16 @@
             :data-auction-id="auction?.id"
             @click="window.dispatchEvent(new CustomEvent('toggle-like', {
                 detail: {
-                auctionId: auction?.id,
-                userId: window.userId,
-                likeId: auction?.likes?.find(like => like.user_id === window.userId)?.id || null
+                    auctionId: auction?.id,
+                    userId: window.userId,
+                    likeId: auction?.likes?.find(like => Number(like.user_id) === Number(window.userId) && String(like.likeable_id) === String(auction?.id))?.id || null
                 }
             }))"
             data-bs-toggle="tooltip"
             data-bs-placement="top"
             title="관심차량을 추가/삭제 합니다"
         >
-        <i class="mdi mdi-heart border-1" :class="auction?.likes?.find(like => like.user_id === window.userId)?.id ? 'text-danger' : 'text-white'"></i>
+            <i class="mdi mdi-heart border-1" :class="auction?.likes?.find(like => Number(like.user_id) === Number(window.userId) && String(like.likeable_id) === String(auction?.id))?.id ? 'text-danger' : 'text-white'"></i>
         </button>
     </div>
 </div>
